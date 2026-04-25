143333b4bf6d493dc230f6a1d9fb3d28fad4ece1
djibyndiaye221 <ndiayedjiby80@gmail.com>
2026-04-25 01:19:19 +0000

    Refactor CV content and update formatting

--- cvdjibyndiaye.py
+++ cvdjibyndiaye.py
@@ -1,9 +1,7 @@
 import streamlit as st
-st.set_page_config(page_title="Mon CV", page_icon="📄", layout="wide")
-st.subheader("expert en Informatique geomatique | Passionné de cybersécurité")
 
 st.markdown("---")
-
+st.header("Profil")
 st.write("""Geomaticien specialiste des etudes geospatial et des cartographies numeriques.Competances averées en informatique et gestion des systemes de données géographiques.Capaciter a travailer en équipe sur des projets d'étude et de realisation de cartographie numerique""")
 
 st.header("Compétences")
@@ -22,7 +20,7 @@ with col2:
 
 st.header(" Expériences professionnelles")
 st.write("""
-**Stagiaire maintenance-service **  
+*Stagiaire maintenance service *  
 - Maintenance informatique  
 - Sécurisation du réseau  
 """)
@@ -30,7 +28,9 @@ st.write("""
 st.header(" Formation")
 st.write("""
 **Licence en Informatique genie logiciel**  
-institut communautaire Africaine de gestion et d'ingeniereie(2022 - 2025)
+institut communautaire Africaine de gestion et d'ingeniereie Amadou Mahtar Mbow
+**Technicien superieur en géomatique**
+Centre d'entreprenariat et de developpement technique G15
 """)
 
 st.header(" Langues")
@@ -39,7 +39,7 @@ st.write("""
  ESPANIOL
 """)
 with st.sidebar:
-    
+    st.image("photodjiby.jpeg")
     st.header("DJIBY NDIAYE")
     st.subheader("Technicien superieur en géomatique") 
     st.write("""
@@ -48,4 +48,4 @@ with st.sidebar:
       
       ndiayedjiby80@email.com   """)
 
-    
+      
